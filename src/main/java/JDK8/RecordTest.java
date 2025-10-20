78d49e8a769cf26b7710c1c9de6d71b7a67b8163
Amanda Grip <amanda.grip@stud.sti.se>
2025-10-20 15:02:43 +0200

    changed to jdk21 and small fix

--- src/main/java/JDK8/RecordTest.java
+++ src/main/java/JDK8/RecordTest.java
@@ -2,5 +2,5 @@ package JDK8;
 
 import javax.validation.constraints.NotBlank; //javax istället för jakarta
 
-public record RecordTest (@NotBlank name, int age) {//fungerar ej då java16+ krävs för record, NotBlank annotationer fungerar inte heller
+public record RecordTest (@NotBlank String name, int age) {//fungerar ej då java16+ krävs för record, NotBlank annotationer fungerar inte heller
 }
