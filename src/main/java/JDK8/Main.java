2882eab815d401c736432fbcb806f7e9771c6247
Amanda Grip <amanda.grip@stud.sti.se>
2025-10-20 14:43:05 +0200

    created main class with switch

---
+++ src/main/java/JDK8/Main.java
@@ -0,0 +1,21 @@
+package JDK8;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = List.of("Alice", "Bob", "Charlie");//.of introducerades senare och fungerar inte i java8
+
+        int day = 3;
+        String result = switch (day) { //fungerar endast i java14+
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            default -> "Unknown";
+        };
+
+        System.out.println(result);
+    }
+}
+
--- src/main/java/JDK8/RecordTest.java
+++ src/main/java/JDK8/RecordTest.java
@@ -1,4 +1,4 @@
 package JDK8;
 
-public record RecordTest (String name, int age) {
+public record RecordTest (String name, int age) {//fungerar ej då java16+ krävs
 }
