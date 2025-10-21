522d1211e41f67aaecd5a9cb3732166489338a54
Amanda Grip <amanda.grip@stud.sti.se>
2025-10-21 14:39:47 +0200

    added gradle project

---
+++ src/main/java/com/example/ee_lektion_2/EeLektion2Application.java
@@ -0,0 +1,13 @@
+package com.example.ee_lektion_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeLektion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeLektion2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ee_lektion_2/EeLektion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ee_lektion_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeLektion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
