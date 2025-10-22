51eb1483b4f0fdf1dc6faf6fc1bb1eae17f446df
Amanda Grip <amanda.grip@stud.sti.se>
2025-10-22 15:31:19 +0200

    created controller
    applied restcontroller and getmapping to two url´s

--- src/main/java/com/amanda/ee_lektion_3/DefaultDuckService.java
+++ src/main/java/com/amanda/ee_lektion_3/DefaultDuckService.java
@@ -1,4 +1,36 @@
 package com.amanda.ee_lektion_3;
 
-public class DefaultDuckService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class DefaultDuckService implements DuckService {
+
+    private final WebClient webClient;
+
+    @Autowired
+    public DefaultDuckService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    @Override
+    public Mono<Duck> getDuck() {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/random")
+                        .build()
+                )
+                .retrieve()
+                .onStatus(
+                        httpStatusCode -> httpStatusCode.is4xxClientError(),
+                        clientResponse -> Mono.error(new RuntimeException("Client Error"))
+                )
+                .onStatus(
+                        httpStatusCode -> httpStatusCode.is5xxServerError(),
+                        clientResponse -> Mono.error(new RuntimeException("Server Error"))
+                )
+                .bodyToMono(Duck.class);
+    }
 }
--- src/main/java/com/amanda/ee_lektion_3/DuckController.java
+++ src/main/java/com/amanda/ee_lektion_3/DuckController.java
@@ -1,4 +1,33 @@
 package com.amanda.ee_lektion_3;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
 public class DuckController {
+
+    private final DuckService duckService;
+
+
+    public DuckController(@Qualifier("defaultDuckService") DuckService duckService) {
+        this.duckService = duckService;
+    }
+
+    @GetMapping("/duck")
+    public Mono<ResponseEntity<Duck>> getDuck(){
+        return duckService.getDuck()
+                .map(ResponseEntity::ok)
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
+
+    @GetMapping("/duck/url")
+    public Mono<ResponseEntity<String>> getDuckUrl(){
+        return duckService.getDuck()
+                .map(duck -> ResponseEntity.ok(duck.getUrl()))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+
+    }
 }
