a29c87d58de6783ebb46c504dd3eb62cee82755b
Amanda Grip <amanda.grip@stud.sti.se>
2025-10-22 15:04:56 +0200

    generated new springboot project

---
+++ src/main/java/com/amanda/ee_lektion_3/EeLektion3Application.java
@@ -0,0 +1,13 @@
+package com.amanda.ee_lektion_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeLektion3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeLektion3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amanda/ee_lektion_3/EeLektion3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amanda.ee_lektion_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeLektion3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
