6017db7f6c232f9040b043151eb6fb8e2ff1b107
Amanda Grip <amanda.grip@stud.sti.se>
2025-10-27 09:40:51 +0100

    generated spring boot project

---
+++ src/main/java/com/example/ee_lektion_4/EeLektion4Application.java
@@ -0,0 +1,13 @@
+package com.example.ee_lektion_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeLektion4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeLektion4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ee_lektion_4/EeLektion4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ee_lektion_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeLektion4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
