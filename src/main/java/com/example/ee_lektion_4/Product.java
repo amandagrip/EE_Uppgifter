a576f9dc9cc4dbc77bffbff07988984bbf46fd0a
Amanda Grip <amanda.grip@stud.sti.se>
2025-11-17 13:32:27 +0100

    created product class with constructor

--- src/main/java/com/example/ee_lektion_4/Product.java
+++ src/main/java/com/example/ee_lektion_4/Product.java
@@ -1,4 +1,50 @@
 package com.example.ee_lektion_4;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.math.BigDecimal;
+
 public class Product {
+
+    private long id;
+
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 2, message = "Name must be at least 2 characters")
+    private String name;
+
+    @NotNull(message = "Price cannot be null")
+    @DecimalMin(value = "0.01", inclusive = true, message = "Price must be at least 0.01")
+    private BigDecimal price;
+
+    public Product() {}
+
+    public Product(long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
+
