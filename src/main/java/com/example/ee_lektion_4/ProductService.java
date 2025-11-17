3ff02b9743e5ade4fb489af15f47525737f81b62
Amanda Grip <amanda.grip@stud.sti.se>
2025-11-17 13:43:37 +0100

    created productservice class

--- src/main/java/com/example/ee_lektion_4/ProductService.java
+++ src/main/java/com/example/ee_lektion_4/ProductService.java
@@ -1,4 +1,17 @@
 package com.example.ee_lektion_4;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
 }
