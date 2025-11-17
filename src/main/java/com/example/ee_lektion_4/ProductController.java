28a8c6e498ebba84e9da36c904e694321fcf070d
Amanda Grip <amanda.grip@stud.sti.se>
2025-11-17 13:49:16 +0100

    created productcontroller
    with postmapping method and returned ResponseEntity

--- src/main/java/com/example/ee_lektion_4/ProductController.java
+++ src/main/java/com/example/ee_lektion_4/ProductController.java
@@ -1,4 +1,25 @@
 package com.example.ee_lektion_4;
 
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) {
+        Product saved = productService.createProduct(product);
+        return ResponseEntity.ok(saved);
+    }
 }
