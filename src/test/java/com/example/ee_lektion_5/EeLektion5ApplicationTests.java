9be4fc5ae114aa691e4d037e7bf8f02ffbd2afd4
Amanda Grip <amanda.grip@stud.sti.se>
2025-11-18 12:20:08 +0100

    initial commit

---
+++ src/test/java/com/example/ee_lektion_5/EeLektion5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ee_lektion_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeLektion5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
